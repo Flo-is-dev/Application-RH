df611f574f03f50b7f0966ea64954ea64701fb40
Flo-le-dev <f.gironde.dev@gmail.com>
2024-08-17 13:32:19 +0200

    date-picker-v1.2

--- src/assets/components/DatePickerContainer.js
+++ src/assets/components/DatePickerContainer.js
@@ -10,6 +10,7 @@ import {
   eachDayOfInterval,
   isSameDay,
   setMonth,
+  setYear,
 } from "date-fns";
 import { FaHome, FaChevronLeft, FaChevronRight } from "react-icons/fa";
 
@@ -18,10 +19,13 @@ const DateInput = () => {
   const [selectedDate, setSelectedDate] = useState(null);
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showMonthSelect, setShowMonthSelect] = useState(false); // État pour contrôler le menu déroulant du mois
+  const [showYearSelect, setShowYearSelect] = useState(false); // État pour contrôler le menu déroulant de l'année
   const dateInputRef = useRef(null);
   const datePickerRef = useRef(null);
   const monthSelectRef = useRef(null); // Référence pour le menu des mois
+  const yearSelectRef = useRef(null); // Référence pour le menu des années
   const monthButtonRef = useRef(null); // Référence pour l'élément du mois
+  const yearButtonRef = useRef(null); // Référence pour l'élément de l'année
 
   const handleDateClick = (day) => {
     setSelectedDate(day);
@@ -56,6 +60,12 @@ const DateInput = () => {
     setShowMonthSelect(false); // Ferme le menu déroulant après sélection
   };
 
+  const handleYearChange = (year) => {
+    const updatedDate = setYear(currentDate, year);
+    setCurrentDate(updatedDate);
+    setShowYearSelect(false); // Ferme le menu déroulant après sélection
+  };
+
   const renderDays = () => {
     const startMonth = startOfMonth(currentDate);
     const endMonth = endOfMonth(currentDate);
@@ -85,10 +95,13 @@ const DateInput = () => {
         datePickerRef.current &&
         !datePickerRef.current.contains(event.target) &&
         monthSelectRef.current &&
-        !monthSelectRef.current.contains(event.target)
+        !monthSelectRef.current.contains(event.target) &&
+        yearSelectRef.current &&
+        !yearSelectRef.current.contains(event.target)
       ) {
         setShowDatePicker(false);
         setShowMonthSelect(false); // Ferme le menu si clic à l'extérieur
+        setShowYearSelect(false); // Ferme le menu des années si clic à l'extérieur
       }
     };
 
@@ -118,6 +131,29 @@ const DateInput = () => {
     </div>
   );
 
+  const renderYearSelect = () => (
+    <div
+      ref={yearSelectRef}
+      className="absolute left-1/2 transform -translate-x-1/2 mt-1 bg-white border border-gray-300 rounded-lg shadow-lg z-20 max-h-48 overflow-y-auto"
+      style={{ width: "90px" }}
+    >
+      {Array.from({ length: 20 }, (_, index) => {
+        const year = 2020 + index;
+        return (
+          <div
+            key={year}
+            onClick={() => handleYearChange(year)}
+            className={`p-2 cursor-pointer hover:bg-gray-200 ${
+              currentDate.getFullYear() === year ? "bg-blue-500 text-white" : ""
+            }`}
+          >
+            {year}
+          </div>
+        );
+      })}
+    </div>
+  );
+
   return (
     <div className="relative inline-block">
       <input
@@ -135,14 +171,25 @@ const DateInput = () => {
         >
           <div className="flex justify-between items-center mb-4 relative">
             <FaChevronLeft className="cursor-pointer" onClick={previousMonth} />
-            <div
-              className="font-bold text-lg cursor-pointer relative"
-              style={{ minWidth: "140px", textAlign: "center" }}
-              onClick={() => setShowMonthSelect(!showMonthSelect)}
-              ref={monthButtonRef}
-            >
-              {format(currentDate, "MMMM yyyy")}
-              {showMonthSelect && renderMonthSelect()}
+            <div className="flex space-x-2">
+              <div
+                className="font-bold text-lg cursor-pointer relative"
+                style={{ minWidth: "95px", textAlign: "center" }}
+                onClick={() => setShowMonthSelect(!showMonthSelect)}
+                ref={monthButtonRef}
+              >
+                {format(currentDate, "MMMM")}
+                {showMonthSelect && renderMonthSelect()}
+              </div>
+              <div
+                className="font-bold text-lg cursor-pointer relative"
+                style={{ textAlign: "center" }}
+                onClick={() => setShowYearSelect(!showYearSelect)}
+                ref={yearButtonRef}
+              >
+                {format(currentDate, "yyyy")}
+                {showYearSelect && renderYearSelect()}
+              </div>
             </div>
             <FaChevronRight className="cursor-pointer" onClick={nextMonth} />
             <FaHome className="cursor-pointer ml-2" onClick={goToToday} />
